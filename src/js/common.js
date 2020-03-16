aa5d94da24e7ea0e7efbd559d431b1320e5af54e
Nikolay Akhmetyanov <nikolay.akhmetyanov@gmail.com>
2020-03-16 14:53:20 +0300

    add legal-form and build

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,41 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	// Custom JS
+	/* Tabs */
+	
+	const rootTabs = document.querySelectorAll("[data-tab='root']"); // Поиск всех табов на странице
 
-});
+	rootTabs.forEach(rootTab => {
+		rootTab.addEventListener('click', handlerTabClick); // Вешаем обработчик на найденные табы
+	})
+
+	// Делегирование обработчика
+	function handlerTabClick(event) { 
+
+		const target = event.target;
+		const navBtns = this.querySelectorAll("[data-tab-nav]");
+		const contents = this.querySelectorAll("[data-tab-content]");
+
+		if (target.getAttribute("data-tab-nav") !== null) {
+			navBtns.forEach(navBtn => {
+				if (navBtn.classList.contains('active')) {
+					navBtn.classList.remove('active');
+				}
+			})
+			target.classList.add('active');
+
+			let valueAttrNavBtn = target.getAttribute("data-tab-nav");
+
+			contents.forEach(content => {
+				if (content.classList.contains('active')) {
+					content.classList.remove('active');
+				}
+				if (content.getAttribute("data-tab-content") === valueAttrNavBtn) {
+					content.classList.add('active');
+				}
+			})
+		}
+
+	}
+	/* Tabs */
+
+})
